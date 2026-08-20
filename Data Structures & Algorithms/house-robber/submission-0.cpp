afaf99ed0b292d7c8d591baa2a576ae8702d8568
Nivedita Shukla <81570025+NS2103@users.noreply.github.com>
2026-08-20 21:15:07 +0900

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rec(int i, vector<int>&nums, vector<int>&dp){
+        if(i>=nums.size())return 0;
+
+        if(dp[i]!=-1)return dp[i];
+
+        int pick=nums[i]+rec(i+2, nums,dp);
+        int notPick= rec(i+1,nums,dp);
+
+        return dp[i]=max(pick,notPick);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return rec(0,nums,dp);
+        
+    }
+};
